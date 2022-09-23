f8a7addaddf702b17cc4299b95218fd461281fae
andresiqueira <andresiqueira-ti@outlook.com>
2022-09-22 23:12:35 -0300

    fix: remove button tag from component

--- src/components/ChatButton/index.tsx
+++ src/components/ChatButton/index.tsx
@@ -5,33 +5,31 @@ export interface ButtonProps {
   message: string
 }
 
-export const ChatButton = ({phone, message}: ButtonProps) => {
+export const ChatButton = ({ phone, message }: ButtonProps) => {
 
   return (
-    <a 
+    <a
       href={`https://api.whatsapp.com/send?phone=${phone}&text=${message}`}
       target="_blank"
       rel="noopener noreferrer"
-      >
-      <button className="
-      flex
-      justify-center
-      items-center
-      bg-green-600
-      text-white
-      w-12
-      h-12
-      rounded-full
-      fixed
-      bottom-14
-      right-8
-      md:bottom-8
-      md:right-10
-      shadow-md
-    ">
+      className="      
+        flex
+        justify-center
+        items-center
+        bg-green-600
+        text-white
+        w-12
+        h-12
+        rounded-full
+        fixed
+        bottom-14
+        right-8
+        md:bottom-8
+        md:right-10
+        shadow-md"
+    >
       <WhatsappLogo size={28} />
       <h6 className="absolute bottom-14 text-xs">Whatsapp</h6>
-    </button>
     </a>
   )
 }
